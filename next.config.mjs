28a3d64a9d6dff4b3528f13da048d67b2376ffad
Kushal Kumar <kushalmalviya1302@gmail.com>
2024-08-10 14:32:40 +0530

    changes config file added env variable

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    SECRET: process.env.SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
+    url: process.env.url,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.envmhFjTPoWv - QYbbn6SWXMIrrJP9Ir,
+    SMTP_USER: process.envg.SMTP_USER,
+    SMTP_PASS: process.env.SMTP_PASS,
+    SMTP_HOST: process.env.SMTP_HOST,
+  },
 };
 
 export default nextConfig;
